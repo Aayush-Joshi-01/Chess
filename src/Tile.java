8a0db1ca7004002c79fa4c6bc2e95980a5658c36
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-09-21 14:46:48 +0530

    Code added

--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,38 @@
-public class Tile {
+public abstract class Tile {
     int tileCoordinate;
+    Tile(int tileCoordinate){
+        this.tileCoordinate=tileCoordinate;
+    }
+    public abstract boolean isTileOccupied();
+    public abstract Piece getPiece();
+    public static final class EmptyTile extends Tile{
+        EmptyTile(int coordinate){
+            super(coordinate);
+        }
+        @Override
+        public boolean isTileOccupied(){
+            return false;
+        }
+        @Override
+        public Piece getPiece(){
+            return null;
+        }
+    }
+    public static final class OccupiedTile extends Tile{
+        Piece pieceOnTile;
+        OccupiedTile(int tileCoordinate, Piece pieceOnTile){
+            super(tileCoordinate);
+            this.tileCoordinate = tileCoordinate;
+        }
 
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
 }
