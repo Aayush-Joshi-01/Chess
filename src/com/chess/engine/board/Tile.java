16e750d7aaaae94ab426354760dc30dbace3801b
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-09-23 09:59:14 +0530

    Restructuring

---
+++ src/com/chess/engine/board/Tile.java
@@ -0,0 +1,53 @@
+package com.chess.engine.board;
+import com.chess.engine.pieces.Piece;
+
+import javax.management.ImmutableDescriptor;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
+    protected final int tileCoordinate;
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles(){\
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();\
+        for(int i = 0; i < 64; i++){
+            emptyTileMap.put(i,new EmptyTile(i));
+        }
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+    Tile(int tileCoordinate){
+        this.tileCoordinate=tileCoordinate;
+    }
+    public abstract boolean isTileOccupied();
+    public abstract Piece getPiece();
+    public static final class EmptyTile extends Tile{
+        EmptyTile(final int coordinate){
+            super(coordinate);
+        }
+        @Override
+        public boolean isTileOccupied(){
+            return false;
+        }
+        @Override
+        public Piece getPiece(){
+            return null;
+        }
+    }
+    public static final class OccupiedTile extends Tile{
+        private final Piece pieceOnTile;
+        OccupiedTile(int tileCoordinate, Piece pieceOnTile, Piece pieceOnTile1){
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
---
+++ src/com/chess/engine/pieces/Piece.java
@@ -0,0 +1,3 @@
+package com.chess.engine.pieces;
+public class Piece {
+}
