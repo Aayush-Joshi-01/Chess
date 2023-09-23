a4d86cbb5ca63d354782d593b1864a111d2631e4
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-09-23 19:56:11 +0530

    Alliance added

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -2,27 +2,30 @@ package com.chess.engine.board;
 import com.chess.engine.pieces.Piece;
 import com.google.common.collect.ImmutableMap;
 
-import javax.management.ImmutableDescriptor;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Tile {
     protected final int tileCoordinate;
-    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
     private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles(){
         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
         for(int i = 0; i < 64; i++){
             emptyTileMap.put(i,new EmptyTile(i));
         }
+        //Collections.unmodifiableMap(emptyTileMap); // This is the inbuilt jdk fucntion instead of using google's guava class and its Immutable function.
         return ImmutableMap.copyOf(emptyTileMap);
     }
-    Tile(int tileCoordinate){
+    public static Tile createTile(final int tileCoordinate, final Piece piece){
+        return piece!=null ? new OccupiedTile(tileCoordinate,piece): EMPTY_TILES_CACHE.get(tileCoordinate);
+    }
+    private Tile(int tileCoordinate){
         this.tileCoordinate=tileCoordinate;
     }
     public abstract boolean isTileOccupied();
     public abstract Piece getPiece();
     public static final class EmptyTile extends Tile{
-        EmptyTile(final int coordinate){
+        private EmptyTile(final int coordinate){
             super(coordinate);
         }
         @Override
@@ -36,7 +39,7 @@ public abstract class Tile {
     }
     public static final class OccupiedTile extends Tile{
         private final Piece pieceOnTile;
-        OccupiedTile(int tileCoordinate, Piece pieceOnTile, Piece pieceOnTile1){
+        private OccupiedTile(int tileCoordinate, Piece pieceOnTile){
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
         }
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,3 +1,13 @@
 package com.chess.engine.pieces;
+import com.chess.engine.Alliance;
+import java.util.List;
 public class Piece {
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+    Piece(final int piecePosition, final Alliance pieceAlliance){
+        this.piecePosition = piecePosition;
+        this.pieceAlliance = pieceAlliance;
+    }
+    public abstract List<Move> calculatedLegalMoves(final Board board);
+
 }
