fcd2b557823c82ad7e7e192a70b123cd0d38abd4
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-09-23 19:10:17 +0530

    Added Guava

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,5 +1,6 @@
 package com.chess.engine.board;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
 
 import javax.management.ImmutableDescriptor;
 import java.util.HashMap;
@@ -8,8 +9,8 @@ import java.util.Map;
 public abstract class Tile {
     protected final int tileCoordinate;
     private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
-    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles(){\
-        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();\
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles(){
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
         for(int i = 0; i < 64; i++){
             emptyTileMap.put(i,new EmptyTile(i));
         }
