17c92c6995b1124c278aabd96754316acb341974
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-09-24 19:43:32 +0530

    Added Move and Board class

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,7 +1,9 @@
 package com.chess.engine.pieces;
 import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
 import java.util.List;
-public class Piece {
+public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
     Piece(final int piecePosition, final Alliance pieceAlliance){
