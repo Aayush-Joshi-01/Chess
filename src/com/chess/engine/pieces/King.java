59763cf0e956c5eb9e11701150d25a453c287ee0
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-10-09 18:17:59 +0530

    Implementing King Class

---
+++ src/com/chess/engine/pieces/King.java
@@ -0,0 +1,25 @@
+package com.chess.engine.pieces;
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class King extends Piece{
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {-9, -8 -7, -1, 1, 7, 8, 9};
+    King(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+    @Override
+    public Collection<Move> calculatedLegalMoves(Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+        int candidateDestinationCoordinate;
+        for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE){
+            candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+}
