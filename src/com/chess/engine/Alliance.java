1c10efbe781218f2f3a8673ecc3e7e20d1beb61f
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-10-09 13:35:03 +0530

    Updated 09102023 1328 hrs

--- src/com/chess/engine/Alliance.java
+++ src/com/chess/engine/Alliance.java
@@ -1,5 +1,16 @@
 package com.chess.engine;
 public enum Alliance { // For Classifying two groups of pieces.
-    WHITE,
-    BLACK
+    WHITE {
+        @Override
+        public int getDirection() {
+            return -1;
+        }
+    },
+    BLACK {
+        @Override
+        public int getDirection() {
+            return 1;
+        }
+    };
+    public abstract int getDirection();
 }
