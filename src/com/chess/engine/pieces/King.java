1b9c99502dbedf131ae596941a5ebb86bdd659ba
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-10-10 01:13:57 +0530

    Cleaned up code

--- src/com/chess/engine/pieces/King.java
+++ src/com/chess/engine/pieces/King.java
@@ -3,11 +3,9 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.google.common.collect.ImmutableList;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 public class King extends Piece{
     private final static int[] CANDIDATE_MOVE_COORDINATE = {-9, -8 -7, -1, 1, 7, 8, 9};
     King(int piecePosition, Alliance pieceAlliance) {
