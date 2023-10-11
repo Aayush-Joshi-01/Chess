e7b8626c4581b2e5e1ce1a2b53e040b520bd3867
Aayush Joshi <aayushjoshi.dev@gmail.com>
2023-10-11 17:36:55 +0530

    Introduced Game Board for initial position of the pieces now next we will implement it

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -1,13 +1,26 @@
 package com.chess.engine.board;
 import com.chess.engine.Alliance;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
+    private final List<Tile> gameboard;
     private Board(Builder builder){
-
+        this.gameboard = createGameBoard(builder);
     }
     public Tile getTile(final int tileCoordinate){
+        return null;
+    }
+    private static List<Tile> createGameBoard(final Builder builder) {
+        final Tile[] tiles = new Tile[BoardUtils.NUM_TILES];
+        for(int i = 0; i < BoardUtils.NUM_TILES; i++){
+            tiles[i] = Tile.createTile(i,builder.boardConfig.get(i));
+        }
+        return ImmutableList.copyOf(tiles);
+    }
+    public static Board createStandardBoard(){
         return null;
     }
     public static class Builder{
