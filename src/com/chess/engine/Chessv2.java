c33223c20530bfbcd6d8e9869b661dc7b924c9f0
Aayush Joshi <76811479+Aayush-Joshi-01@users.noreply.github.com>
2023-10-23 01:10:21 +0530

    code cleanup

--- src/com/chess/engine/Chessv2.java
+++ src/com/chess/engine/Chessv2.java
@@ -3,7 +3,7 @@ package com.chess.engine;
 import com.chess.engine.board.Board;
 
 public class Chessv2 {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Board board = Board.createStandardBoard();
         System.out.println(board);
     }
