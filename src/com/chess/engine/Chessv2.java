08f56155c6b85fce001261dfa55360f1f470b10d
Aayush Joshi <76811479+Aayush-Joshi-01@users.noreply.github.com>
2023-10-23 01:15:30 +0530

    Commented

--- src/com/chess/engine/Chessv2.java
+++ src/com/chess/engine/Chessv2.java
@@ -1,10 +1,11 @@
 package com.chess.engine;
-
 import com.chess.engine.board.Board;
-
 public class Chessv2 {
     public static void main(String[] args){
         Board board = Board.createStandardBoard();
+        //Driver Code for the Chess.
+        //This will be configured when we publish the application.
         System.out.println(board);
+        //Prints out the board.
     }
 }
