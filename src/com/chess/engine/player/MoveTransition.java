6a38deea6ae2f56dcc9ffb29175d8f4dd2e81227
Aayush Joshi <76811479+Aayush-Joshi-01@users.noreply.github.com>
2023-11-17 23:25:30 +0530

    Streak

--- src/com/chess/engine/player/MoveTransition.java
+++ src/com/chess/engine/player/MoveTransition.java
@@ -5,7 +5,6 @@ public class MoveTransition {
     private final Board transitionBoard;
     private final Move move;
     private final MoveStatus moveStatus;
-
     public MoveTransition(Board transitionBoard, Move move, MoveStatus moveStatus) {
         this.transitionBoard = transitionBoard;
         this.move = move;
